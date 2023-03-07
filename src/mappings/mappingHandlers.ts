87c83f4aa1932a7ad071451ca7bfd8547de52ae4
Andriy <andriy.vasyltsiv@gmail.com>
2023-03-07 17:50:15 +0200

    saving extrinsics_hash functionality was added to transfer events

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,40 +3,38 @@ import {
   SubstrateEvent,
   SubstrateBlock,
 } from "@subql/types";
-import { StarterEntity } from "../types";
+import { TransferEntity } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  //Create a new starterEntity with ID using block hash
-  let record = new StarterEntity(block.block.header.hash.toString());
-  //Record block number
-  record.field1 = block.block.header.number.toNumber();
-  await record.save();
-}
+export async function handleEventTransfer(event: SubstrateEvent): Promise<void> {
+  logger.info("handleEventTransfer called!")
+  
+  logger.info("{new event call" + JSON.stringify(event) + "}")
+  const from = event.event.data[0];
+  const to = event.event.data[1];
+  const amount = event.event.data[2];
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  const {
-    event: {
-      data: [account, balance],
-    },
-  } = event;
-  //Retrieve the record by its ID
-  const record = await StarterEntity.get(
-    event.block.block.header.hash.toString()
-  );
-  record.field2 = account.toString();
-  //Big integer type Balance of a transfer event
-  record.field3 = (balance as Balance).toBigInt();
-  await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-  const record = await StarterEntity.get(
-    extrinsic.block.block.header.hash.toString()
-  );
-  //Date type timestamp
-  record.field4 = extrinsic.block.timestamp;
-  //Boolean tyep
-  record.field5 = true;
-  await record.save();
+  // Create the new transfer entity
+  var id = event.block.block.header.number.toNumber() + event.idx
+  var blockNumber = event.block.block.header.number.toBigInt();
+  var from_ = from.toString();
+  var to_ = to.toString();
+  var ext = event.extrinsic.extrinsic.hash;
+  var success = event.extrinsic.success
+  var Bamount = (amount as Balance).toBigInt();
+  logger.info(
+    "\t\tid: " + id +
+    "\n\t\t blockN: " + blockNumber.toString() +
+    "\n\t\tFrom: " + from_ +
+    "\n\t\tto:" + to_ +
+    "\n\t\tAmount: " + Bamount.toString() +
+    "\n\t\tExtr: " + ext)
+  const transfer = new TransferEntity(event.block.block.header.number.toNumber() + "-" + event.idx);
+  transfer.Amount = Bamount;
+  transfer.BlockNumber = blockNumber;
+  transfer.From = from_;
+  transfer.To = to_;
+  transfer.ExtrinsicsHash = ext.toString();
+  transfer.Success = success;
+  await transfer.save();
 }
